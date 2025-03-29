7e7859bb4a2236610b3a12fbe03e72fab5d4b3f2
Raj Seth <sethraj03@gmail.com>
2025-03-30 04:24:43 +0530

    Added login Production

--- src/Utils/Global.ts
+++ src/Utils/Global.ts
@@ -1,7 +1,7 @@
 const DEV = "http://localhost:3000/api";
 const PRODUCTION = "https://autograde-k1ci.onrender.com/api";
 
-export const BASE_URL = DEV;
+export const BASE_URL = PRODUCTION;
 export const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZTRmNmRhODE0YTZkNTRkNmY3OGU0YyIsImlhdCI6MTc0MzI3MDU4MH0.A8wQPQOnudNEXg0hzgqDjnAYZeTqxQufuMy9_zwrGo4";
 
